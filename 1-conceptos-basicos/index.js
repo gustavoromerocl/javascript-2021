a5c20cb649051efe65f726ba3cfd20f033a99da1
romerodevelopments <gandresrp@gmail.com>
2021-12-14 13:32:10 -0300

    conceptos basicos - numeros y operaciones aritmeticas

--- 1-conceptos-basicos/index.js
+++ 1-conceptos-basicos/index.js
@@ -1,3 +1,5 @@
+/**Variables y contantes en javascript */
+
 let nombre = "Gustavo";
 
 nombre = 2.2;
@@ -7,3 +9,10 @@ console.log(nombre);
 const PI = 3.1416;
 
 //PI = "gustavo" ERROR
+
+/**Números y operaciones aritméticas */
+
+// + - * / % Math.pi Math.pow Math.round Math.sqrt
+
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math
+
